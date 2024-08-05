526338f5573ca7f9703e58148a4fb98924bdf1f1
manolis.perrot <manolis.perrot@univ-grenoble-alpes.fr>
2024-08-05 16:37:12 +0200

    test params

--- tests/scripts/WANG1_edmf_vs_les.py
+++ tests/scripts/WANG1_edmf_vs_les.py
@@ -119,46 +119,76 @@ common_params = {
     'entr_scheme': 'R10',
     'Cent': 0.99,
     'Cdet': 1.99,       # 'Cdet': 2.5,
-    'wp_a': 0.2,
-    'wp_b': 0.2,      # 'wp_b': 1.
+    'wp_a': 1.,
+    'wp_b': 1.,      # 'wp_b': 1.
     'wp_bp': 0.003*250,     #      0.002,
-    'up_c': 0.25,
-    'vp_c': 0.25,
+    'up_c': 0.2,
+    'vp_c': 0.2,
     'bc_ap': 0.2,    #0.3,
-    'delta_bkg': 0.0045*250,   # 0.006,
+    'delta_bkg': 0.02*250,   # 0.02,
     'output_filename': 'run',
     'wp0':-1.e-08,
     'write_netcdf': True
 }
+
+# Good for theta and velocities: 
+    # 'Cent': 0.99,
+    # 'Cdet': 1.99,       # 'Cdet': 2.5,
+    # 'wp_a': 1.,
+    # 'wp_b': 1.,      # 'wp_b': 1.
+    # 'wp_bp': 0.003*250,     #      0.002,
+    # 'up_c': 0.2,
+    # 'vp_c': 0.2,
+    # 'bc_ap': 0.2,    #0.3,
+    # 'delta_bkg': 0.02*250,   # 0.02,
+    # 'output_filename': 'run',
+    # 'wp0':-1.e-08,
+
+
 
 common_params.update(case_params[case])  # Update with the specific case configuration in case_params[case]
 
 
 # Define parameters specific to each run (overwrite common parameters):
 
-run_label = ['ED+EVD',  'EDMF-Energy','EDMF-Energy-cor' ]
+# run_label = ['ED+EVD',  'EDMF-Energy','EDMF-Energy-cor' ]
+# runs = [
+#     {
+#         'eddy_diff': True,
+#         'evd': True,
+#         'mass_flux_tra': False,
+#         'mass_flux_dyn': False,
+#         'mass_flux_tke': False,
+#         'mass_flux_tke_trplCorr': False,
+#         'output_filename': 'run1.nc',
+#     'write_netcdf': False
+#     },
+#             {
+#         'eddy_diff': True,
+#         'evd': False,
+#         'mass_flux_tra': True,
+#         'mass_flux_dyn': True,
+#         'mass_flux_tke': True,
+#         'mass_flux_tke_trplCorr': True,
+#         'entr_scheme': 'R10',
+#         'output_filename': 'run2.nc',
+#     'write_netcdf': False
+#     },
+#         {
+#         'eddy_diff': True,
+#         'evd': False,
+#         'mass_flux_tra': True,
+#         'mass_flux_dyn': True,
+#         'mass_flux_tke': True,
+#         'mass_flux_tke_trplCorr': True,
+#         'entr_scheme': 'R10corNT',
+#         'output_filename': 'scm_WANG1_FR.nc',
+#     'write_netcdf': True
+#     }
+#         ]
+
+run_label = ['EDMF-Energy-cor' ]
 runs = [
-    {
-        'eddy_diff': True,
-        'evd': True,
-        'mass_flux_tra': False,
-        'mass_flux_dyn': False,
-        'mass_flux_tke': False,
-        'mass_flux_tke_trplCorr': False,
-        'output_filename': 'run1.nc',
-    'write_netcdf': False
-    },
-            {
-        'eddy_diff': True,
-        'evd': False,
-        'mass_flux_tra': True,
-        'mass_flux_dyn': True,
-        'mass_flux_tke': True,
-        'mass_flux_tke_trplCorr': True,
-        'entr_scheme': 'R10',
-        'output_filename': 'run2.nc',
-    'write_netcdf': False
-    },
         {
         'eddy_diff': True,
         'evd': False,
@@ -171,7 +201,6 @@ runs = [
     'write_netcdf': True
     }
         ]
-
 
 
 
@@ -216,206 +245,225 @@ alpha_les = 1
 linewidth_les = 4
 
 
+if True:
+    #============================================ WC ===============================================
+    fig, axes = plt.subplots(nrows=3, ncols=3, sharex=False,
+                            sharey=True, figsize=(15, 12))
 
-#============================================ WC ===============================================
-fig, axes = plt.subplots(nrows=3, ncols=3, sharex=False,
-                         sharey=True, figsize=(15, 12))
+    ax_index=-1
 
-ax_index=-1
-
-# ===============================================================
-ax_index+=1
-ax = axes.flat[ax_index]
-ax.set_xlabel(r'$^{\circ}{\rm C}$')
-ax.set_ylabel(r'$z / h $')
-ax.set_title(r'$\overline{\theta}$')
+    # ===============================================================
+    ax_index+=1
+    ax = axes.flat[ax_index]
+    ax.set_xlabel(r'$^{\circ}{\rm C}$')
+    ax.set_ylabel(r'$z / h $')
+    ax.set_title(r'$\overline{\theta}$')
 
 
-ax.plot(TH_les[instant], z_r_les/mld, style_les,
-        alpha=alpha_les, linewidth=linewidth_les,  label='LES')
+    ax.plot(TH_les[instant], z_r_les/mld, style_les,
+            alpha=alpha_les, linewidth=linewidth_les,  label='LES')
 
-for i, label in enumerate(run_label):
-    ax.plot(scm[i].t_np1[:, 0], scm[i].z_r/mld, linestyle=styles[i], color = colors[i],
+    for i, label in enumerate(run_label):
+        ax.plot(scm[i].t_np1[:, 0], scm[i].z_r/mld, linestyle=styles[i], color = colors[i],
+                alpha=alpha[i], linewidth=linewidth[i], label=label)
+
+    #ax.set_xlim((1.55, 1.8))
+    #ax.set_ylim((-1.3, 0))
+
+
+    # ===============================================================
+
+    ax_index+=1
+    ax = axes.flat[ax_index]
+    ax.set_title(r'$\overline{u}$')
+
+    ax.set_xlabel(r'${\rm m}\;{\rm s}^{-1}$')
+
+    ax.plot(U_les[instant], z_r_les/mld, style_les,
+            alpha=alpha_les, linewidth=linewidth_les, label='LES')
+
+    for i, label in enumerate(run_label):
+        ax.plot((scm[i].u_np1), scm[i].z_r/mld, linestyle=styles[i], color = colors[i],
+                alpha=alpha[i], linewidth=linewidth[i], label=label)
+        # if i != 0: 
+        #     ax.plot((out[i].w_p[instant]), out[i].z_w/mld, linestyle=':', color = colors[i],
+        #         alpha=alpha[i], linewidth=linewidth[i], label=label)
+
+    ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+    #ax.set_ylim((-1.3, 0))
+
+
+    # ===============================================================
+
+
+    ax_index+=1
+    ax = axes.flat[ax_index]
+
+    ax.set_title(r'$\overline{w^\prime \theta^\prime}$')
+
+    ax.plot(WTH[instant], z_r_les/mld, style_les,
+            alpha=alpha_les, linewidth=linewidth_les, label='LES')
+
+    #for i, label in enumerate(run_label):
+    #    if run_label == 'ED':
+    #        ax.plot(-(scm[i].wted), scm[i].z_w/mld, styles[i], color = colors[i],
+    #                alpha=alpha[i], linewidth=linewidth[i], label=label)
+    #    else:
+    #        ax.plot(-(scm[i].wted + scm[i].wtmf), scm[i].z_w/mld, styles[i], color = colors[i],
+    #                alpha=alpha[i], linewidth=linewidth[i], label=label)
+    #ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+    #for i, label in enumerate(run_label):
+    #    if run_label == 'ED':
+    #        ax.plot(-(scm[i].wted), scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
+    #                alpha=alpha[i], linewidth=linewidth[i], label=label)
+    #    else:
+    #        ax.plot(-(scm[i].wted + scm[i].wtmf), scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
+    #                alpha=alpha[i], linewidth=linewidth[i], label=label)
+    #ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+
+    #ax.plot( -out[1]['WT_ED'][-1,:], out[1].z_w/mld, color ='tab:blue'  , linestyle ='--', alpha=1.0 , linewidth=3 )
+    #ax.plot( -out[2]['WT_ED'][-1,:], out[2].z_w/mld, color ='tab:orange', linestyle ='--', alpha=1.0 , linewidth=3 )
+
+
+    #ax.set_ylim((-1.3, 0))
+
+    ax.set_xlabel(r'${\rm K}\;{\rm m}\;{\rm s}^{-1}$')
+
+
+    # ===============================================================
+    # ===============================================================
+    ax_index+=1
+    ax = axes.flat[ax_index]
+    ax.set_title(r'$k$')
+
+    ax.plot(TKE[instant], z_r_les/mld, style_les,
+            alpha=alpha_les, linewidth=linewidth_les, label='LES')
+
+    for i, label in enumerate(run_label):
+        ax.plot(scm[i].tke_np1, scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
+                alpha=alpha[i], linewidth=linewidth[i], label=label)
+    ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+
+    #ax.set_xlim((-0.0001, 0.001))
+    #ax.set_ylim((-1.3, 0))
+
+    ax.set_xlabel(r'${\rm m}^2\;{\rm s}^{-2}$')
+    # ===============================================================
+
+
+    ax_index+=1
+    ax = axes.flat[ax_index]
+    ax.set_title(r'$\overline{w^\prime \frac{u^{\prime 2}}{2}  }$')
+
+    ax.plot(WTKE[instant], z_r_les/mld, style_les,
+            alpha=alpha_les, linewidth=linewidth_les, label='LES')
+
+    for i, label in enumerate(run_label):
+        ax.plot((scm[i].wtke), scm[i].z_r/mld, linestyle=styles[i], color = colors[i],
+                alpha=alpha[i], linewidth=linewidth[i], label=label)
+        
+    # if i==1:
+    #     ax.plot((out[i]['we'][instant]), out[i].z_w/mld, linestyle=':', color = colors[i],
+    #             alpha=alpha[i], linewidth=linewidth[i], label=label)
+
+
+    #ax.set_xlim((- 1e-5, 0))
+    #ax.set_ylim((-1.3, 0))
+
+    ax.set_xlabel(r'${\rm m}^3\;{\rm s}^{-3}$')
+
+    # ===============================================================
+    ax_index+=1
+    ax = axes.flat[ax_index]
+
+    ax.set_xlabel(r'${\rm m}^2\;{\rm s}^{-2}$')
+    ax.set_title(r'$\overline{w^\prime u^\prime}$')
+
+    ax.plot(-WU[instant], z_r_les/mld, style_les,alpha=alpha_les, linewidth=linewidth_les, label='LES')
+
+
+    # for i, label in enumerate(run_label):
+    #         ax.plot((scm[i].wued + scm[i].wumf), scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
+    #                 alpha=alpha[i], linewidth=linewidth[i], label=label)
+
+    # ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+
+    # ax.plot( scm[1].wued, scm[1].z_w/mld, color ='tab:blue'  , linestyle ='--', alpha=1.0 , linewidth=3 )
+    # ax.plot( scm[2].wued, scm[2].z_w/mld, color ='tab:orange', linestyle ='--', alpha=1.0 , linewidth=3 )
+
+    #ax.set_ylim((-1.3, 0))
+
+    # ===============================================================
+    ax_index+=1
+    ax = axes.flat[ax_index]
+
+    ax.set_xlabel(r'$s^{-1}$')
+    ax.set_title(r'$\omega_p$')
+
+
+    for i, label in enumerate(run_label):
+        if label=='EDMF-Energy-cor':
+            ax.plot((scm[i].vortp), scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
+                alpha=alpha[i], linewidth=linewidth[i], label=label)
+
+    #ax.set_ylim((-1.3, 0))
+
+    # ===============================================================
+
+    ax_index+=1
+    ax = axes.flat[ax_index]
+
+    ax.set_xlabel(r'$m s^{-1}$')
+    ax.set_title(r'$w_p$')
+
+
+    for i, label in enumerate(run_label):
+        if (label=='EDMF-Energy-cor') or (label=='EDMF-Energy'):
+            ax.plot((scm[i].wp), scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
+                alpha=alpha[i], linewidth=linewidth[i], label=label)
+
+    #ax.set_ylim((-1.3, 0))
+
+    # ===============================================================
+    # ===============================================================
+
+    ax_index+=1
+    ax = axes.flat[ax_index]
+
+    ax.set_xlabel(r'$m s^{-1}$')
+    ax.set_title(r'$w_p$')
+
+    for i, label in enumerate(run_label):
+        if (label=='EDMF-Energy-cor') or (label=='EDMF-Energy'):
+            ax.plot((scm[i].ent), scm[i].z_r/mld, linestyle='-', color = colors[i],
+                alpha=alpha[i], linewidth=linewidth[i], label=label)
+            ax.plot((scm[i].det), scm[i].z_r/mld, linestyle='--', color = colors[i],
+                alpha=alpha[i], linewidth=linewidth[i], label=label)
+            ax.plot((scm[i].ent-scm[i].det), scm[i].z_r/mld, linestyle=':', color = colors[i],
             alpha=alpha[i], linewidth=linewidth[i], label=label)
+    ax.set_xlim(-0.01,0.01)
+    #ax.set_ylim((-1.3, 0))
 
-#ax.set_xlim((1.55, 1.8))
-#ax.set_ylim((-1.3, 0))
+    # ===============================================================
 
+    # adding subplot labels
+    subplot_label = [r'\rm{(a)}', r'\rm{(b)}', r'\rm{(c)}',
+                    r'\rm{(d)}', r'\rm{(e)}', r'\rm{(f)}',r'\rm{(g)}',r'\rm{(h)}',r'\rm{(i)}']
 
-# ===============================================================
-
-ax_index+=1
-ax = axes.flat[ax_index]
-ax.set_title(r'$\overline{u}$')
-
-ax.set_xlabel(r'${\rm m}\;{\rm s}^{-1}$')
-
-ax.plot(U_les[instant], z_r_les/mld, style_les,
-        alpha=alpha_les, linewidth=linewidth_les, label='LES')
-
-for i, label in enumerate(run_label):
-    ax.plot((scm[i].u_np1), scm[i].z_r/mld, linestyle=styles[i], color = colors[i],
-            alpha=alpha[i], linewidth=linewidth[i], label=label)
-    # if i != 0: 
-    #     ax.plot((out[i].w_p[instant]), out[i].z_w/mld, linestyle=':', color = colors[i],
-    #         alpha=alpha[i], linewidth=linewidth[i], label=label)
-
-ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-#ax.set_ylim((-1.3, 0))
-
-
-# ===============================================================
-
-
-ax_index+=1
-ax = axes.flat[ax_index]
-
-ax.set_title(r'$\overline{w^\prime \theta^\prime}$')
-
-ax.plot(WTH[instant], z_r_les/mld, style_les,
-        alpha=alpha_les, linewidth=linewidth_les, label='LES')
-
-#for i, label in enumerate(run_label):
-#    if run_label == 'ED':
-#        ax.plot(-(scm[i].wted), scm[i].z_w/mld, styles[i], color = colors[i],
-#                alpha=alpha[i], linewidth=linewidth[i], label=label)
-#    else:
-#        ax.plot(-(scm[i].wted + scm[i].wtmf), scm[i].z_w/mld, styles[i], color = colors[i],
-#                alpha=alpha[i], linewidth=linewidth[i], label=label)
-#ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-#for i, label in enumerate(run_label):
-#    if run_label == 'ED':
-#        ax.plot(-(scm[i].wted), scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
-#                alpha=alpha[i], linewidth=linewidth[i], label=label)
-#    else:
-#        ax.plot(-(scm[i].wted + scm[i].wtmf), scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
-#                alpha=alpha[i], linewidth=linewidth[i], label=label)
-#ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-
-#ax.plot( -out[1]['WT_ED'][-1,:], out[1].z_w/mld, color ='tab:blue'  , linestyle ='--', alpha=1.0 , linewidth=3 )
-#ax.plot( -out[2]['WT_ED'][-1,:], out[2].z_w/mld, color ='tab:orange', linestyle ='--', alpha=1.0 , linewidth=3 )
-
-
-#ax.set_ylim((-1.3, 0))
-
-ax.set_xlabel(r'${\rm K}\;{\rm m}\;{\rm s}^{-1}$')
-
-
-# ===============================================================
-# ===============================================================
-ax_index+=1
-ax = axes.flat[ax_index]
-ax.set_title(r'$k$')
-
-ax.plot(TKE[instant], z_r_les/mld, style_les,
-        alpha=alpha_les, linewidth=linewidth_les, label='LES')
-
-for i, label in enumerate(run_label):
-    ax.plot(scm[i].tke_np1, scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
-            alpha=alpha[i], linewidth=linewidth[i], label=label)
-ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-
-#ax.set_xlim((-0.0001, 0.001))
-#ax.set_ylim((-1.3, 0))
-
-ax.set_xlabel(r'${\rm m}^2\;{\rm s}^{-2}$')
-# ===============================================================
-
-
-ax_index+=1
-ax = axes.flat[ax_index]
-ax.set_title(r'$\overline{w^\prime \frac{u^{\prime 2}}{2}  }$')
-
-ax.plot(WTKE[instant], z_r_les/mld, style_les,
-        alpha=alpha_les, linewidth=linewidth_les, label='LES')
-
-for i, label in enumerate(run_label):
-    ax.plot((scm[i].wtke), scm[i].z_r/mld, linestyle=styles[i], color = colors[i],
-            alpha=alpha[i], linewidth=linewidth[i], label=label)
-    
-# if i==1:
-#     ax.plot((out[i]['we'][instant]), out[i].z_w/mld, linestyle=':', color = colors[i],
-#             alpha=alpha[i], linewidth=linewidth[i], label=label)
-
-
-#ax.set_xlim((- 1e-5, 0))
-#ax.set_ylim((-1.3, 0))
-
-ax.set_xlabel(r'${\rm m}^3\;{\rm s}^{-3}$')
-
-# ===============================================================
-ax_index+=1
-ax = axes.flat[ax_index]
-
-ax.set_xlabel(r'${\rm m}^2\;{\rm s}^{-2}$')
-ax.set_title(r'$\overline{w^\prime u^\prime}$')
-
-ax.plot(-WU[instant], z_r_les/mld, style_les,alpha=alpha_les, linewidth=linewidth_les, label='LES')
-
-
-# for i, label in enumerate(run_label):
-#         ax.plot((scm[i].wued + scm[i].wumf), scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
-#                 alpha=alpha[i], linewidth=linewidth[i], label=label)
-
-# ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-
-# ax.plot( scm[1].wued, scm[1].z_w/mld, color ='tab:blue'  , linestyle ='--', alpha=1.0 , linewidth=3 )
-# ax.plot( scm[2].wued, scm[2].z_w/mld, color ='tab:orange', linestyle ='--', alpha=1.0 , linewidth=3 )
-
-#ax.set_ylim((-1.3, 0))
-
-# ===============================================================
-ax_index+=1
-ax = axes.flat[ax_index]
-
-ax.set_xlabel(r'$s^{-1}$')
-ax.set_title(r'$\omega_p$')
-
-
-for i, label in enumerate(run_label):
-    if label=='EDMF-Energy-cor':
-        ax.plot((scm[i].vortp), scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
-            alpha=alpha[i], linewidth=linewidth[i], label=label)
-
-#ax.set_ylim((-1.3, 0))
-
-# ===============================================================
-
-ax_index+=1
-ax = axes.flat[ax_index]
-
-ax.set_xlabel(r'$m s^{-1}$')
-ax.set_title(r'$w_p$')
-
-
-for i, label in enumerate(run_label):
-    if (label=='EDMF-Energy-cor') or (label=='EDMF-Energy'):
-        ax.plot((scm[i].wp), scm[i].z_w/mld, linestyle=styles[i], color = colors[i],
-            alpha=alpha[i], linewidth=linewidth[i], label=label)
-
-#ax.set_ylim((-1.3, 0))
-
-# ===============================================================
-
-
-# adding subplot labels
-subplot_label = [r'\rm{(a)}', r'\rm{(b)}', r'\rm{(c)}',
-                r'\rm{(d)}', r'\rm{(e)}', r'\rm{(f)}',r'\rm{(g)}',r'\rm{(h)}',r'\rm{(i)}']
-
-for i,ax in enumerate(axes.flat):
-    # ax.set_ylim((-2, 0))
-    ax.set_box_aspect(1)
-    ax.text(0.15, 0.98, subplot_label[i], transform=ax.transAxes,
-            fontsize=16, bbox=dict(facecolor='1.', edgecolor='none', pad=3.0), fontweight='bold', va='top', ha='right')
+    for i,ax in enumerate(axes.flat):
+        # ax.set_ylim((-2, 0))
+        ax.set_box_aspect(1)
+        ax.text(0.15, 0.98, subplot_label[i], transform=ax.transAxes,
+                fontsize=16, bbox=dict(facecolor='1.', edgecolor='none', pad=3.0), fontweight='bold', va='top', ha='right')
 
 
 
 
-handles, labels = axes.flat[0].get_legend_handles_labels()
-fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(
-    0.5, -0.05), fancybox=False, shadow=False, ncol=4)
+    handles, labels = axes.flat[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(
+        0.5, -0.05), fancybox=False, shadow=False, ncol=4)
 
-fig.tight_layout()
+    fig.tight_layout()
 
 
 
